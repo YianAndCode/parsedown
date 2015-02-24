ac771fa8b868a83c6f3f3ee1c6f43f9709d8f6dc
yian <yian0418@gmail.com>
2015-02-24 10:20:41 +0800

    调整 html 转义逻辑，对全局进行 html 转义

--- Parsedown.php
+++ Parsedown.php
@@ -23,6 +23,8 @@ class Parsedown
 
     function text($text)
     {
+        #我自己加的
+        $text = htmlspecialchars($text, ENT_NOQUOTES, 'UTF-8');
         # make sure no definitions are set
         $this->DefinitionData = array();
 
@@ -340,7 +342,7 @@ class Parsedown
     {
         $text = $Block['element']['text']['text'];
 
-        $text = htmlspecialchars($text, ENT_NOQUOTES, 'UTF-8');
+        //$text = htmlspecialchars($text, ENT_NOQUOTES, 'UTF-8');
 
         $Block['element']['text']['text'] = $text;
 
@@ -455,7 +457,7 @@ class Parsedown
     {
         $text = $Block['element']['text']['text'];
 
-        $text = htmlspecialchars($text, ENT_NOQUOTES, 'UTF-8');
+        //$text = htmlspecialchars($text, ENT_NOQUOTES, 'UTF-8');
 
         $Block['element']['text']['text'] = $text;
 
@@ -1054,7 +1056,7 @@ class Parsedown
         if (preg_match('/^('.$marker.'+)[ ]*(.+?)[ ]*(?<!'.$marker.')\1(?!'.$marker.')/s', $Excerpt['text'], $matches))
         {
             $text = $matches[2];
-            $text = htmlspecialchars($text, ENT_NOQUOTES, 'UTF-8');
+            //$text = htmlspecialchars($text, ENT_NOQUOTES, 'UTF-8');
             $text = preg_replace("/[ ]*\n/", ' ', $text);
 
             return array(
